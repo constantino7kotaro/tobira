2c3417bbfe0550835015b1c9ce4bbbd404fa4496
EC2 Default User <ec2-user@ip-172-31-38-170.ap-southeast-1.compute.internal>
2019-01-14 04:46:35 +0000

    user_controller 初期画像の設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,12 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(nickname: params[:nickname], email: params[:email], password: params[:password])
+    @user = User.new(nickname: params[:nickname], 
+                     email: params[:email], 
+                     password: params[:password]
+                     # NOTE: 初期画像のファイルはまだ保存していない
+                      image_name: "default_user.jpg"
+                     )
     if @user.save
 #TODO: 新規登録でデータが保存された後は、ログイン後のtopページであるホスト一覧画面（スライドのp.14)へリダイレクト 
 #NOTE: 現段階ではひとまずユーザ詳細ページへリダイレクト
