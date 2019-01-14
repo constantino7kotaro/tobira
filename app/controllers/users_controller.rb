fbac532c7b2cc5b110a20e6ef4a638af2dc19b50
EC2 Default User <ec2-user@ip-172-31-38-170.ap-southeast-1.compute.internal>
2019-01-14 02:50:51 +0000

    user_controllerの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,20 @@ class UsersController < ApplicationController
   def index
     @users = User.all.order('updated_at DESC')
   end
-end
 
   def show
     @user = User.find_by(id: params[:id])
   end
 
+  def new
+  end
+  
+  def create
+    @user = User.new(nickname: params[:nickname], email: params[:email], password: params[:password])
+    @user.save
+#TODO: 新規登録でデータが保存された後は、ログイン後のtopページであるホスト一覧画面（スライドのp.14)へリダイレクト 
+#NOTE: 現段階ではひとまずユーザ詳細ページへリダイレクト
+    redirect_to("/users/#{@user.id}")
+  end
 
+end
