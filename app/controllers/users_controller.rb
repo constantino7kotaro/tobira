5087d75d01ba57f664bfa4f678785cba5597f1aa
EC2 Default User <ec2-user@ip-172-31-38-170.ap-southeast-1.compute.internal>
2019-01-14 13:08:35 +0000

    users_controller#showの修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,20 +3,8 @@ class UsersController < ApplicationController
     @users = User.all.order('updated_at DESC')
   end
 
-  def new
-    @user = User.new
+  def show
+    @user = User.find(id: params[:id])
   end
-  
-  def create
-    @user = User.new(nickname: params[:nickname], email: params[:email], password: params[:password])
-    if @user.save
-  #TODO: 新規登録でデータが保存された後は、ログイン後のtopページであるホスト一覧画面（スライドのp.14)へリダイレクト 
-  #NOTE: 現段階ではひとまずユーザ詳細ページへリダイレクト
-    redirect_to("/users/#{@user.id}")
-    flash[:notice] = "ユーザー登録が完了しました"
-    else
-      render("users/new")
-    end 
-  end
-
 end
+
