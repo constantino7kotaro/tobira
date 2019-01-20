c753895321c2a87c5b3b79510b5632e5d32b4cfc
EC2 Default User <ec2-user@ip-172-31-38-170.ap-southeast-1.compute.internal>
2019-01-20 14:07:14 +0000

    users_controller & new.html.erb修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,28 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def create
+    p '================='
+    p params
+    p '=================='
+    # paramsの中のuserの中のnicknameを取得したい時
+    # params[:user][:nickname]
+    @user = User.new(nickname: params[:user][:nickname], 
+                    email: params[:user][:email], 
+                    password: params[:user][:password])
+    p '================='
+    p @user
+    p '=================='
+    if @user.save
+    #TODO: 新規登録でデータが保存された後は、ログイン後のtopページであるホスト一覧画面（スライドのp.14)へリダイレクト 
+    #NOTE: 現段階ではひとまずユーザ詳細ページへリダイレクト
+    redirect_to("/users/#{@user.id}")
+    flash[:notice] = "ユーザー登録が完了しました"
+    else
+      render("users/new")
+    end 
+  end
+  
   def show
     @user = User.find(params[:id])
   end
