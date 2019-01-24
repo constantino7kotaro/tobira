5b907436a8949440599f28e256e3b4f39599a100
EC2 Default User <ec2-user@ip-172-31-38-170.ap-southeast-1.compute.internal>
2019-01-24 13:35:01 +0000

    migrateファイルの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_133617) do
+ActiveRecord::Schema.define(version: 2019_01_22_133132) do
 
   create_table "users", force: :cascade do |t|
     t.string "nickname"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_01_22_133617) do
     t.string "email"
     t.string "password"
     t.string "age"
+    t.string "image_age"
   end
 
 end
