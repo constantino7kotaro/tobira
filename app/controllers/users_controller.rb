dcb99e4db01f29fae98996036765ea1016b4343d
EC2 Default User <ec2-user@ip-172-31-38-170.ap-southeast-1.compute.internal>
2019-01-25 14:23:32 +0000

    masterの戻し

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,8 @@ class UsersController < ApplicationController
   def index
     @users = User.all.order('updated_at DESC')
   end
-
-  def edit
-    @user = User.find(id: params[:id])
-  end
-
-  def update
-    @user = User.find(id: params[:id])
-    @user.nickname = params[:nickname]
-    @user.introduction = params[:introduction]
-    
-    if params[:image]
-      @user.image_name = "#{@user.id}.jpg"
-      image = params[:image]
-      File.binwrite("public/user_images/#{@user.image_name}", image.read )
-    end
-    
-    if @user.save
-      flash[:notice] = "プロフィールを編集しました"
-      redirect_to("/users/#{@user.id}")
-    else
-      render("users/profile_edit")
-    end
+  
+  def show
+    @user = User.find(params[:id])
   end
 end
