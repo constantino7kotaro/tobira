ebc0c57ffbaca3c16299cf0f6d721b8e07150d27
前川晃一郎 <qian41119122@gmail.com>
2019-01-29 00:47:24 +0900

    maekawa test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,21 +6,25 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+
   def create
-    @user = User.new(nickname: params[:user][:nickname], 
-                    email: params[:user][:email], 
+    @user = User.new(nickname: params[:user][:nickname],
+                    email: params[:user][:email],
                     password: params[:user][:password])
     if @user.save
-      #TODO: 新規登録でデータが保存された後は、ログイン後のtopページであるホスト一覧画面（スライドのp.14)へリダイレクト 
+      #TODO: 新規登録でデータが保存された後は、ログイン後のtopページであるホスト一覧画面（スライドのp.14)へリダイレクト
       #NOTE: 現段階ではひとまずユーザ詳細ページへリダイレクト
       redirect_to("/users/#{@user.id}")
       flash[:notice] = "ユーザー登録が完了しました"
     else
       render("users/new")
-    end 
+    end
   end
-  
+
+  def edit
+    @user = User.find(id: params[:id])
+  end
+
   def show
     @user = User.find(params[:id])
   end
