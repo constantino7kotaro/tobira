b3d89869b6e281f13825d13fd8a72eaad468e635
EC2 Default User <ec2-user@ip-172-31-38-170.ap-southeast-1.compute.internal>
2019-02-09 16:22:52 +0000

    validatesの追加

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,4 +1,10 @@
 class Lesson < ApplicationRecord
+    validates :user_id, {presence: true, uniqueness: true}
+    validates :skill_id, {presence: true}
+    validates :area_id, {presence: true}
+    validates :lesson_type_id, {presence: true}
+    validates :content, {length: {maximum: 140}}
+    
     def user
      return User.find(self.user_id)
     end
