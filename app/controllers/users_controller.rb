80087242ccfedcf5130b22d4ac9f604f6d7f08a9
EC2 Default User <ec2-user@ip-172-31-38-170.ap-southeast-1.compute.internal>
2019-02-19 14:15:38 +0000

    ユーザ登録時のログイン機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,8 @@ class UsersController < ApplicationController
                     #TODO:初期画像が設定されるようにする
                     #image_name: "default_user.jpg"
     if @user.save
-      #TODO: 新規登録でデータが保存された後は、ログイン後のtopページであるホスト一覧画面（スライドのp.14)へリダイレクト
-      #NOTE: 現段階ではひとまずユーザ詳細ページへリダイレクト
-      redirect_to("/users/#{@user.id}")
+      session[:user_id] = @user.id
+      redirect_to("/lessons/index")
       flash[:notice] = "ユーザー登録が完了しました"
     else
       render("users/new")
