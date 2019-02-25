4b0eeb0a69c82a369b9950cff357632fd7d5f7ed
EC2 Default User <ec2-user@ip-172-31-38-170.ap-southeast-1.compute.internal>
2019-02-25 13:18:26 +0000

    8文字以上のパスワード制限 validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     validates :nickname, {presence: true, uniqueness: true}
     validates :email, {presence: true, uniqueness: true}
+    validates :password_digest, {presence: true, length: {minimum: 8}}
 end
