5f2521c35f5f7f1b689bf78c0b726877b3e171c8
EC2 Default User <ec2-user@ip-172-31-38-170.ap-southeast-1.compute.internal>
2019-02-28 14:18:05 +0000

    controller修正

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+    before_action :authenticate_user
+    before_action :ensure_correct_user, {only: [:destroy_form, :destroy]}
+    
     def index
         @lessons = Lesson.all.order("updated_at DESC")
     end
@@ -12,16 +15,34 @@ class LessonsController < ApplicationController
     end
     
     def create
-        @lesson = Lesson.new(user_id: 1,
-                         area_id: params[:lesson][:area_id], 
-                         skill_id: params[:lesson][:skill_id], 
-                         lesson_type_id: params[:lesson][:lesson_type_id], 
-                         content: params[:lesson][:content])
+        @lesson = Lesson.new(user_id: @current_user.id,
+                            area_id: params[:lesson][:area_id], 
+                            skill_id: params[:lesson][:skill_id], 
+                            lesson_type_id: params[:lesson][:lesson_type_id], 
+                            content: params[:lesson][:content])
         if @lesson.save
             redirect_to("/lessons/#{@lesson.id}")
             flash[:notice] = "ホスト登録が完了しました"
         else
             render("lessons/new")
+        end
+    end
+    
+    def destroy_form
+        @lesson = Lesson.find(params[:id])
+    end
+    
+    def destroy
+        @lesson = Lesson.find(params[:id])
+        @lesson.destroy
+        redirect_to("/lessons/index")
+    end
+    
+    def ensure_correct_user
+        @lesson = Lesson.find(params[:id])
+        if @current_user.id != @lesson.user_id 
+            flash[:notice] = "権限がありません"
+            redirect_to("/lessons/index")
         end
     end
     
